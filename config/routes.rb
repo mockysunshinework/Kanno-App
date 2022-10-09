e0de640608c974c3c9c2c1b81f6c97ff7e77c4ab
mockysunshinework <mockysunshinework@gmail.com>
2022-10-09 12:14:35 +0900

    viewとcontrollerのカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
     omniauth_callbacks: "omniauth_callbacks"
   }
   root 'static_pages#top'
