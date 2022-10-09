9acf25d33e6fe5a2036ac8a7d80416d25a930574
mockysunshinework <mockysunshinework@gmail.com>
2022-10-09 19:31:19 +0900

    routes.rbに resources :users, :only => [:index, :show] を追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     omniauth_callbacks: "omniauth_callbacks"
   }
   root 'static_pages#top'
+  resources :users, :only => [:index, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
