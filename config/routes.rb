e6533a799e29956231192de11d428be03b9adfcb
mockysunshinework <mockysunshinework@gmail.com>
2022-10-16 20:04:37 +0900

    loginページを未ログイン時のトップページに設定。application_controllerno のbefore_action:authenticate_useronly: [:top, :index, :show]を記述。ルートにroot to: ~を記述。これでログインと新規登録画面のベースができた

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,19 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     omniauth_callbacks: "omniauth_callbacks"
   }
-  devise_scope :user do
+  root to: 'static_pages#top' 
+
+
+  # devise_scope :user do
     # TIPS: ユーザー登録しっぱいのリダイレクトのエラーを防ぐ https://github.com/heartcombo/devise/blob/master/app/controllers/devise/registrations_controller.rb
     # get '/users', to: 'users/registrations#new'
     # or
-    get '/users', to: redirect('/users/sign_up')
-  end
-  root 'static_pages#top'
+    # get '/users', to: redirect('/users/sign_up')
+  # end
+
   resources :users, :only => [:index, :show]
+
+ 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
