b1ddb852aaab752bab4b95befbd95828d7b7e34e
mockysunshinework <mockysunshinework@gmail.com>
2022-10-17 12:28:30 +0900

    マイグレーションファイルにnameカラムが重複していたエラーを解消

--- db/schema.rb
+++ db/schema.rb
@@ -18,11 +18,11 @@ ActiveRecord::Schema.define(version: 20221005020624) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
