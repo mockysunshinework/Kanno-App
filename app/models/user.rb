37c44f09c0640e5d33cde8a5d82c067dec4462e6
mockysunshinework <mockysunshinework@gmail.com>
2022-10-19 06:04:22 +0900

    フォームにdepartmentを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,12 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 100 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
+  validates :department, length: { in: 2..30 }, allow_blank: true                    
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable
   devise :database_authenticatable, :registerable,
--- db/migrate/20221005020624_add_column_to_users.rb
+++ db/migrate/20221005020624_add_column_to_users.rb
@@ -4,6 +4,7 @@ class AddColumnToUsers < ActiveRecord::Migration[5.1]
     add_column :users, :provider, :string
     add_column :users, :uid, :string
     add_column :users, :name, :string
+    add_column :users, :department, :string
     add_column :users, :admin, :boolean, default: false
   end
 end
