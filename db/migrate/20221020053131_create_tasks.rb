f2b2d5fcb3fc87ce07458961fd1bc97d0ae7a415
mockysunshinework <mockysunshinework@gmail.com>
2022-10-27 20:19:13 +0900

    Taskモデルからrequest系のカラムを削除

--- db/migrate/20221020053131_create_tasks.rb
+++ db/migrate/20221020053131_create_tasks.rb
@@ -5,9 +5,6 @@ class CreateTasks < ActiveRecord::Migration[5.1]
       t.string :description
       t.string :status, default: "未"
       t.datetime :deadline
-      t.string :request_name
-      t.string :request_description
-
       t.references :user, foreign_key: true
 
       t.timestamps
