4c3c663f5e56de1050d70363e9216fe7c2a4221d
mockysunshinework <mockysunshinework@gmail.com>
2022-10-30 05:23:38 +0900

    seedに記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,28 @@ User.create!( name: "Sample User",
     					password_confirmation: "password",
     					admin: true)
 
-User.create!( name: "Sample User",
+User.create!( name: "岸田A",
     					email: "sample-1@email.com",
     					password: "password",
     					password_confirmation: "password",
     					department: "group1",
               superior: true)
 
-User.create!( name: "Sample User",
+User.create!( name: "岸田B",
     					email: "sample-2@email.com",
     					password: "password",
     					password_confirmation: "password",
     					department: "group1",
               superior: true)
 
-User.create!( name: "Sample User",
+User.create!( name: "安倍A",
               email: "sample-3@email.com",
               password: "password",
               password_confirmation: "password",
               department: "group2",
               superior: true)             
                 
-User.create!( name: "Sample User",
+User.create!( name: "安倍B",
               email: "sample-4@email.com",
               password: "password",
               password_confirmation: "password",
