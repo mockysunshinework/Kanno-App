d826a3cac326f35d1674f0b6c90d3cd0c7025c9e
mockysunshinework <mockysunshinework@gmail.com>
2022-11-07 15:02:41 +0900

    機能の実装を再度やり直したところ

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,7 +4,7 @@ class Request < ApplicationRecord
   validates :request_name, presence: true
   validates :request_description, presence: true  
 
-  validate :request_time_check
+  # validate :request_time_check
 
   def request_time_check
     errors.add(request_deadline, "は本日以降を選択してください。") if self.request_deadline < DateTime.now
