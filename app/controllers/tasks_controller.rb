907311ecc17bf30f67a0bc2ad62df5b029e9782e
mockysunshinework <mockysunshinework@gmail.com>
2022-11-10 17:47:46 +0900

    taskコントローラーeditアクションのロジックを修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -32,13 +32,21 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    if params[:task][:deadline].to_date < @task.created_at.to_date
-      flash[:danger] = "期日はタスクの作成日以降の日付にして下さい"
-      render :edit      
-    else
+    if params[:task][:deadline].nil? || params[:task][:deadline].blank?
       @task.update(task_update_params)
       @task.save
-      redirect_to user_tasks_url @user
+      flash[:success] = "タスク情報を変更しました。"      
+      redirect_to user_tasks_url @user      
+    else
+      if params[:task][:deadline].to_date < @task.created_at.to_date
+        flash[:danger] = "期日はタスクの作成日以降の日付にして下さい"
+        render :edit      
+      else
+        @task.update(task_update_params)
+        @task.save
+        flash[:success] = "タスク情報を変更しました。"
+        redirect_to user_tasks_url @user
+      end
     end
   end
 
