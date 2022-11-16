f98a01383eeced93d86b3ff06ef139f3e7fe7f49
mockysunshinework <mockysunshinework@gmail.com>
2022-11-16 15:51:58 +0900

    seedsのdepartment 文字数を８文字以上に変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,32 +4,33 @@ User.create!( name: "Sample User",
     					email: "sample@email.com",
     					password: "password",
     					password_confirmation: "password",
+              department: "group_number1",
     					admin: true)
 
-User.create!( name: "岸田A",
+User.create!( name: "サンプル太郎",
     					email: "sample-1@email.com",
     					password: "password",
     					password_confirmation: "password",
-    					department: "group1",
+    					department: "group_number1",
               partner: true)
 
-User.create!( name: "岸田B",
+User.create!( name: "サンプル姫子",
     					email: "sample-2@email.com",
     					password: "password",
     					password_confirmation: "password",
-    					department: "group1",
+    					department: "group_number1",
               partner: true)
 
-User.create!( name: "安倍A",
+User.create!( name: "テスト太郎",
               email: "sample-3@email.com",
               password: "password",
               password_confirmation: "password",
-              department: "group2",
+              department: "group_number2",
               partner: true)             
                 
-User.create!( name: "安倍B",
+User.create!( name: "テスト姫子",
               email: "sample-4@email.com",
               password: "password",
               password_confirmation: "password",
-              department: "group2",
+              department: "group_number2",
               partner: true)                   
